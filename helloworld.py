5b6492f0cfdbcc3d512201bfb1edf49f6765f9a7
Jackie Dallas <jackie.r.dallas@gmail.com>
2022-01-29 23:45:06 -0600

    learning about variables in pythond

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+print("Hello Python")
+# user_name = input("What is your name: ")
+# user_age = input("How old are you: ")
+# print(user_name + " is " + user_age + " years old.")
+
+# a = 10
+# id(10)
+# print(id(10))
+
+progress = "I love writing Python code!"
+print(progress)
