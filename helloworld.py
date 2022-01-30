02dd842ef84d170b50991c875be9a393f404f96d
Jackie Dallas <jackie.r.dallas@gmail.com>
2022-01-30 00:30:10 -0600

    practicing string manipulation

--- helloworld.py
+++ helloworld.py
@@ -9,3 +9,20 @@ print("Hello Python")
 
 progress = "I love writing Python code!"
 print(progress)
+
+my_string = "this is my string"
+string1 = "Cisco Router"
+print(string1[0])
+print(string1[-1])
+# print(len(string1))
+string_length = len(string1)
+print(string_length)
+a = "Cisco Switch"
+print(a.index('w'))
+print(my_string.find('is'))
+print(my_string.upper())
+print(my_string.join('_'))
+print(progress.index("w"))
+print(len(progress))
+print(progress[7: 27])
+
